69d48221043a45c570925e1185b293647debcf1d
Jorge Bustamante <94580142+jorgebustmonttec@users.noreply.github.com>
2024-04-30 14:25:50 -0600

    created app

---
+++ APP/CopaFrisa/CopaFrisa/CopaFrisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopaFrisaApp.swift
+//  CopaFrisa
+//
+//  Created by Jorge Bustamante on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CopaFrisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
