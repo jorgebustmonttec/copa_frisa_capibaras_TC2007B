26e9f93fcd1d2a0ba73e2bc640244d27f470fb7f
Jorge Bustamante <94580142+jorgebustmonttec@users.noreply.github.com>
2024-06-04 22:07:24 -0600

    ssl working

--- API/app.js
+++ API/app.js
@@ -1,16 +1,16 @@
-//app.js
-
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const jugadorRoutes = require('./routes/jugadorRoutes');
 const equipoRoutes = require('./routes/equipoRoutes');
 
-
 const app = express();
-const port = 3000;
-
+//const port = 3000;
+const httpsPort = 3443;
 
 const options = {
   definition: {
@@ -21,31 +21,41 @@ const options = {
           description: 'A simple Express User API',
       },
       servers: [
+          //{
+              //url: 'http://localhost:3000',
+          //},
           {
-              url: 'http://localhost:3000',
-          },
+              url: 'https://localhost:3443',
+          }
       ],
   },
-  apis: ['API/routes/**/*.js'], // Note the addition of 'API/' before 'routes/'
+  apis: ['API/routes/**/*.js'],
 };
-
 
 const swaggerSpec = swaggerJSDoc(options);
 
 app.use(express.json());
 
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 
 app.use('/usuarios', usuarioRoutes);
 app.use('/jugadores', jugadorRoutes);
 app.use('/equipos', equipoRoutes);
 
-
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+// Update paths for SSL files
+const sslPath = path.join(__dirname, 'ssl');
+const sslOptions = {
+  key: fs.readFileSync(path.join(sslPath, 'key.pem')),
+  cert: fs.readFileSync(path.join(sslPath, 'cert.pem'))
+};
+
+https.createServer(sslOptions, app).listen(httpsPort, () => {
+  console.log(`Server listening on https://localhost:${httpsPort}!`);
+});
+
+// now using https
+//app.listen(port, () => console.log(`Server listening on http://localhost:${port}!`));
 
 module.exports = app;
