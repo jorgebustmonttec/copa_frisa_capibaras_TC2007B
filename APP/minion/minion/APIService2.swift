a7f85206397cacfd0d17b240748539fbefabf713
Jorge Bustamante <94580142+jorgebustmonttec@users.noreply.github.com>
2024-06-09 13:15:25 -0600

    minor mods app

---
+++ APP/minion/minion/APIService2.swift
@@ -0,0 +1,22 @@
+//
+//  APIService2.swift
+//  minion
+//
+//  Created by Jorge Bustamante on 09/06/24.
+//
+
+import Foundation
+
+
+
+
+class APIService2 {
+    static let shared = APIService2()
+
+    
+
+
+    
+
+}
+
