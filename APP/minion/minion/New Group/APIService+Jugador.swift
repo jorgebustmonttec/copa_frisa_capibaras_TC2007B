540b244cba1c16d9c1c636914a5534a83276794d
Jorge Bustamante <94580142+jorgebustmonttec@users.noreply.github.com>
2024-06-09 23:25:23 -0600

    busqueda full

--- APP/minion/minion/New Group/APIService+Jugador.swift
+++ APP/minion/minion/New Group/APIService+Jugador.swift
@@ -7,3 +7,39 @@
 // APIService+Jugador.swift
 import Foundation
 
+extension APIService {
+    func fetchAllJugadores(url: String, completion: @escaping (Result<[APIJugadorEquipo], APIError>) -> Void) {
+           guard let url = URL(string: url) else {
+               completion(.failure(.invalidURL))
+               return
+           }
+
+           var request = URLRequest(url: url)
+           request.httpMethod = "GET"
+
+           let session = URLSession(configuration: .default, delegate: SelfSignedCertificateDelegate(), delegateQueue: nil)
+           let task = session.dataTask(with: request) { data, response, error in
+               if let error = error {
+                   completion(.failure(.requestFailed))
+                   return
+               }
+
+               guard let data = data else {
+                   completion(.failure(.requestFailed))
+                   return
+               }
+
+               do {
+                   let jugadores = try JSONDecoder().decode([APIJugadorEquipo].self, from: data)
+                   DispatchQueue.main.async {
+                       completion(.success(jugadores))
+                   }
+               } catch {
+                   completion(.failure(.decodingFailed("Failed to decode jugadores response.")))
+               }
+           }
+           task.resume()
+       }
+
+
+}
--- apptesting/grrrrrr/grrrrrr/MatchTest.swift
+++ apptesting/grrrrrr/grrrrrr/MatchTest.swift
@@ -1,224 +1,98 @@
 //
-//  MatchTest.swift
-//  grrrrrr
+//  StatsView.swift
+//  App
 //
-//  Created by Jorge Bustamante on 09/06/24.
+//  Created by Gael Gaytan on 04/06/24.
 //
+
 import SwiftUI
 
-struct APIPartido: Codable, Identifiable {
-    var id: Int { id_partido }
-    var id_partido: Int
-    var equipo_a: Int
-    var equipo_b: Int
-    var fecha: String
-}
-
-struct APIEquipo: Codable {
-    var id_equipo: Int
-    var nombre_equipo: String
-    var escudo: Data?
-}
-
-struct APIGoals: Codable {
-    var total_goals: Int
-}
-
-struct MatchTest: View {
-    @State private var pastPartidos: [APIPartido] = []
-    @State private var futurePartidos: [APIPartido] = []
-    @State private var equipos: [APIEquipo] = []
-    @State private var goals: [String: Int] = [:] // Store goals for teams with match ID as key
-    @State private var errorMessage: String = ""
+struct StatsView: View {
+    var team1: String
+    var logo1: String
+    var team2: String
+    var logo2: String
+    var goalsTeam1: [(player: String, minute: Int)]
+    var goalsTeam2: [(player: String, minute: Int)]
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-
-            VStack {
-                Image("copa")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 100)
-                    .padding(.top)
-
-             //   Text("Partidos")
-                  //  .font(.largeTitle)
-                    //.fontWeight(.bold)
-                    //.padding()
-
-                if !errorMessage.isEmpty {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .padding()
-                }
-
-                VStack {
-                    Text("Partidos Pasados")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .padding()
-                    ScrollView {
-                        ForEach(pastPartidos) { partido in
-                            partidoView(partido: partido)
-                        }
-                    }
-                    .frame(height: 150)
-                    .background(Color.gray.opacity(0.1))
-                    .cornerRadius(10)
-                }
+        VStack {
+            Text("Estadísticas del partido")
+                .font(.title)
+                .fontWeight(.bold)
                 .padding()
-
-                VStack {
-                    Text("Partidos Futuros")
+            
+            HStack(spacing: 30) {
+                VStack(alignment: .leading) { // Alineación izquierda para el equipo 1
+                    Image(logo1)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Text(team1)
                         .font(.headline)
-                        .fontWeight(.bold)
-                        .padding()
-                    ScrollView {
-                        ForEach(futurePartidos) { partido in
-                            partidoView(partido: partido)
+                        .padding(.bottom)
+                    
+                    ForEach(goalsTeam1.indices, id: \.self) { index in
+                        let goal = goalsTeam1[index]
+                        HStack {
+                            Image(systemName: "person.circle")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(.blue)
+                                .frame(width: 20, height: 20)
+                            
+                            Text("\(goal.player) (\(goal.minute)')")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
                         }
+                        .padding(.vertical, 5)
                     }
-                    .frame(height: 150)
-                    .background(Color.gray.opacity(0.1))
-                    .cornerRadius(10)
                 }
+                
+                Spacer() // Añadir espacio para alinear verticalmente
+                
+                VStack(alignment: .leading) { // Alineación izquierda para el equipo 2
+                    Image(logo2)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Text(team2)
+                        .font(.headline)
+                        .padding(.bottom)
+                    
+                    ForEach(goalsTeam2.indices, id: \.self) { index in
+                        let goal = goalsTeam2[index]
+                        HStack {
+                            Image(systemName: "person.circle")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(.blue)
+                                .frame(width: 20, height: 20)
+                            
+                            Text("\(goal.player) (\(goal.minute)')")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.vertical, 5)
+                    }
+                }
+            }
+            .padding()
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(15)
+            .padding()
+            
+            // Imagen de la cancha de fútbol
+            Image("cancha_futbol2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
                 .padding()
-            }
-        .onAppear {
-            fetchEquipos()
-            fetchPastPartidos()
-            fetchFuturePartidos()
-        }
-    }
-
-    @ViewBuilder
-    private func partidoView(partido: APIPartido) -> some View {
-        VStack(alignment: .leading) {
-            Text("Fecha: \(formatDate(partido.fecha))")
-            HStack {
-                if let equipoA = getEquipo(by: partido.equipo_a) {
-                    if let escudoData = equipoA.escudo, let escudo = UIImage(data: escudoData) {
-                        Image(uiImage: escudo)
-                            .resizable()
-                            .frame(width: 50, height: 50)
-                    }
-                    Text("Equipo A: \(equipoA.nombre_equipo)")
-                }
-                Text("Goles A: \(goals["\(partido.id_partido)_\(partido.equipo_a)"] ?? 0)")
-            }
-            HStack {
-                if let equipoB = getEquipo(by: partido.equipo_b) {
-                    if let escudoData = equipoB.escudo, let escudo = UIImage(data: escudoData) {
-                        Image(uiImage: escudo)
-                            .resizable()
-                            .frame(width: 50, height: 50)
-                    }
-                    Text("Equipo B: \(equipoB.nombre_equipo)")
-                }
-                Text("Goles B: \(goals["\(partido.id_partido)_\(partido.equipo_b)"] ?? 0)")
-            }
-        }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(10)
-        .shadow(radius: 3)
-        .padding([.bottom, .horizontal])
-    }
-
-    private func fetchPastPartidos() {
-        fetchPartidos(url: "https://localhost:3443/partidos/pasados") { partidos in
-            self.pastPartidos = partidos
-            for partido in partidos {
-                fetchGoals(for: partido.equipo_a, in: partido.id_partido)
-                fetchGoals(for: partido.equipo_b, in: partido.id_partido)
-                fetchEquipoShield(by: partido.equipo_a)
-                fetchEquipoShield(by: partido.equipo_b)
+            
             }
         }
     }
 
-    private func fetchFuturePartidos() {
-        fetchPartidos(url: "https://localhost:3443/partidos/futuros") { partidos in
-            self.futurePartidos = partidos
-            for partido in partidos {
-                fetchGoals(for: partido.equipo_a, in: partido.id_partido)
-                fetchGoals(for: partido.equipo_b, in: partido.id_partido)
-                fetchEquipoShield(by: partido.equipo_a)
-                fetchEquipoShield(by: partido.equipo_b)
-            }
-        }
-    }
-
-    private func fetchPartidos(url: String, completion: @escaping ([APIPartido]) -> Void) {
-        APIService.shared.fetchPartidos(url: url) { result in
-            switch result {
-            case .success(let partidos):
-                completion(partidos)
-            case .failure(let error):
-                self.errorMessage = error.localizedDescription
-            }
-        }
-    }
-
-    private func fetchGoals(for teamId: Int, in matchId: Int) {
-        let url = "https://localhost:3443/puntos/goles/equipo/\(teamId)/partido/\(matchId)/total"
-        APIService.shared.fetchGoals(url: url) { result in
-            switch result {
-            case .success(let goals):
-                DispatchQueue.main.async {
-                    self.goals["\(matchId)_\(teamId)"] = goals.total_goals
-                }
-            case .failure(let error):
-                self.errorMessage = error.localizedDescription
-            }
-        }
-    }
-
-    private func fetchEquipoShield(by teamId: Int) {
-        let url = "https://localhost:3443/equipos/single/\(teamId)/escudo"
-        APIService.shared.fetchEquipoShield(url: url) { result in
-            switch result {
-            case .success(let shieldData):
-                if let index = equipos.firstIndex(where: { $0.id_equipo == teamId }) {
-                    DispatchQueue.main.async {
-                        self.equipos[index].escudo = shieldData
-                    }
-                }
-            case .failure(let error):
-                self.errorMessage = error.localizedDescription
-            }
-        }
-    }
-
-    private func fetchEquipos() {
-        APIService.shared.fetchEquipos { result in
-            switch result {
-            case .success(let equipos):
-                self.equipos = equipos
-            case .failure(let error):
-                self.errorMessage = error.localizedDescription
-            }
-        }
-    }
-
-    private func formatDate(_ dateStr: String) -> String {
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
-        if let date = formatter.date(from: dateStr) {
-            let displayFormatter = DateFormatter()
-            displayFormatter.dateStyle = .medium
-            displayFormatter.timeStyle = .short
-            return displayFormatter.string(from: date)
-        }
-        return dateStr
-    }
-
-    private func getEquipo(by id: Int) -> APIEquipo? {
-        return equipos.first { $0.id_equipo == id }
-    }
-}
-
-struct MatchTest_Previews: PreviewProvider {
+struct StatsView_Previews: PreviewProvider {
     static var previews: some View {
-        MatchTest()
+        StatsView(team1: "Equipo A", logo1: "logo1", team2: "Equipo B", logo2: "logo2", goalsTeam1: [("Jugador1", 10), ("Jugador2", 25)], goalsTeam2: [("Jugador3", 35), ("Jugador4", 45)])
     }
 }
