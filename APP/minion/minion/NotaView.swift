6d34b232666fa84a02a085e8defd5ae16c52ac67
Jorge Bustamante <94580142+jorgebustmonttec@users.noreply.github.com>
2024-06-10 04:18:07 -0600

    publicaciones full

--- APP/minion/minion/NotaView.swift
+++ APP/minion/minion/NotaView.swift
@@ -4,41 +4,64 @@
 //
 //  Created by Miguel Ponce on 16/05/24.
 //
-
 import SwiftUI
 
 struct NotaView: View {
     var title: String
-    var imageName: String
+    var image: Image?
     var message: String
+    var date: String
+    var author: String
 
     var body: some View {
-        VStack {
-            Text(title)
-                .font(.title)
-                .fontWeight(.bold)
-                .padding()
-            
-            Image(imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(8)
-                .shadow(radius: 4)
-                .padding()
-            
-            Text(message)
-                .font(.body)
-                .padding()
-
-            Spacer()
+        ScrollView{
+            VStack {
+                Text(title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                if let image = image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(8)
+                        .shadow(radius: 4)
+                        .padding()
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(8)
+                        .shadow(radius: 4)
+                        .padding()
+                }
+                
+                HStack {
+                    Text(date)
+                        .font(.subheadline)
+                    Text("•")
+                        .font(.subheadline)
+                    Text(author)
+                        .font(.subheadline)
+                        .bold()
+                }
+                .padding(.bottom, 10)
+                
+                Text(message)
+                    .font(.body)
+                    .padding()
+                
+                Spacer()
+            }
         }
-        .navigationBarTitle(Text(title), displayMode: .inline)
+        .navigationBarTitle("", displayMode: .inline)
         .padding()
     }
 }
 
 struct NotaView_Previews: PreviewProvider {
     static var previews: some View {
-        NotaView(title: "Titulo Ejemplo", imageName: "REGLAS", message: "Detalles del ejemplo aquí")
+        NotaView(title: "Titulo Ejemplo", image: nil, message: "Detalles del ejemplo aquí", date: "2024-06-08", author: "admin")
     }
 }
